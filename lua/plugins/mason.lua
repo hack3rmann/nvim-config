8f9fb330b956d56f47873d0dac373bbb0d686bfa
Svyatoslav <hack3rmann@github.com>
2024-10-01 17:41:13 +0300

    `mason` added

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+    "williamboman/mason.nvim",
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    },
+    config = function(_, opts)
+        require("mason").setup()
+    end,
+}
