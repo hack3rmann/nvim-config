c0c0edfa838656ea1f4bfda703f9b0a3a77d6f96
Svyatoslav <hack3rmann@github.com>
2024-10-03 20:23:31 +0300

    Formatting on save disabled (and can be configured in `switches`)

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,17 +2,16 @@ return {
     "nvimtools/none-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local null_ls = require "null-ls"
-        null_ls.setup {
+        local null_ls = require("null-ls")
+        local switches = require("config.switches")
+
+        null_ls.setup({
             on_attach = function(client, bufnr)
-                if client.supports_method "textDocument/formatting" then
-                    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+                if switches.format_on_save and client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ buffer = bufnr })
                     vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
                         buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format()
-                        end,
+                        callback = vim.lsp.buf.format,
                     })
                 end
             end,
@@ -20,6 +19,6 @@ return {
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.stylua,
             },
-        }
+        })
     end,
 }
