ecb0d1071c5073e69780adbc0ac618fc89ed0361
Svyatoslav <hack3rmann@github.com>
2024-10-04 20:53:43 +0300

    Added keymap to hide search highlight

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,3 +2,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open default filesystem"
 vim.keymap.set("n", "<leader>a", vim.lsp.handlers["textDocument/codeAction"], { desc = "Open code actions" })
 vim.keymap.set("n", "<leader>gd", vim.lsp.handlers["textDocument/definition"], { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>gr", vim.lsp.handlers["textDocument/references"], { desc = "Go to references" })
+vim.keymap.set("n", "<leader>/", ":noh<CR>", { silent = true, desc = "Clear search highlight" })
