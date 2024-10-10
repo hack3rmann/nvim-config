82963198f98c6876a73620fe0fffad1f4074e5a4
Svyatoslav <hack3rmann@github.com>
2024-10-10 09:58:32 +0300

    rustc "discover" added to rust-analyzer

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -61,6 +61,7 @@ return {
                         command = "clippy",
                         features = "all",
                     },
+                    rustc = { source = "discover" },
                 },
             },
         })
