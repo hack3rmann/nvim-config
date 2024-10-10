5878b2a225a14c08787fbf194aea88869cd7f278
Svyatoslav <hack3rmann@github.com>
2024-10-10 12:40:29 +0300

    Code actions fixed

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,18 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open default filesystem" })
-vim.keymap.set("n", "<leader>a", vim.lsp.handlers["textDocument/codeAction"], { desc = "Open code actions" })
-vim.keymap.set("n", "<leader>gd", vim.lsp.handlers["textDocument/definition"], { desc = "Go to definition" })
-vim.keymap.set("n", "<leader>gr", vim.lsp.handlers["textDocument/references"], { desc = "Go to references" })
+-- Default NeoVim file tree
+vim.keymap.set("n", "<leader>fs", vim.cmd.Ex, { desc = "Open default filesystem" })
+
+-- LSP code actions
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Open code actions" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "<leader>gr", function(context, opts)
+    local has_telescope, telescope = pcall(require, "telescope.builtin")
+
+    if has_telescope then
+        telescope.lsp_references()
+    else
+        vim.lsp.buf.references(context, opts)
+    end
+end, { desc = "Go to references" })
+
+-- Disable search highlight
 vim.keymap.set("n", "<leader>/", ":noh<CR>", { silent = true, desc = "Clear search highlight" })
