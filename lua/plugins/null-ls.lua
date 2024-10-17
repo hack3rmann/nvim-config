e0622f2ad080601cc458a51d68088c64c9462df4
Svyatoslav <slava-varov3@mail.ru>
2024-10-17 04:36:00 +0300

    Yaml formatter

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -22,6 +22,7 @@ return {
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.nixfmt,
+                null_ls.builtins.formatting.yamlfmt,
             },
         })
 
