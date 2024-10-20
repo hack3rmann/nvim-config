a1cc1a9041a328cb54a4059ef6fc6974ed09349e
Svyatoslav <slava30727@github.com>
2024-10-20 15:10:14 +0000

    remap hop.nvim to `<leader>v`

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -10,7 +10,7 @@ return {
 
         local Direcion = require("hop.hint").HintDirection
 
-        vim.keymap.set("", "<leader>h", function()
+        vim.keymap.set("", "<leader>v", function()
             hop.hint_patterns({ direction = Direcion.ALL, current_line_only = false })
         end, { remap = true, desc = "Hop jump" })
     end,
