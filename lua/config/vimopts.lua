548a8def22f794f7df84d3d18b3ef0978ed3db2a
Svyatoslav <slava-varov3@mail.ru>
2024-12-07 20:14:57 +0300

    add hyprland `.conf` filetypes

--- lua/config/vimopts.lua
+++ lua/config/vimopts.lua
@@ -13,3 +13,12 @@ vim.cmd([[hi CursorLineNr guifg=#af00af]])
 vim.opt.scrolloff = 10
 
 vim.g.mapleader = " "
+
+vim.filetype.add({
+    pattern = {
+        [".*/waybar/config"] = "jsonc",
+        [".*/mako/config"] = "dosini",
+        [".*/kitty/*.conf"] = "bash",
+        [".*/hypr/.*%.conf"] = "hyprlang",
+    },
+})
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -19,10 +19,13 @@ return {
                 end
             end,
             sources = {
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.nixfmt,
                 null_ls.builtins.formatting.yamlfmt,
+                null_ls.builtins.formatting.cmakelang,
             },
         })
 
