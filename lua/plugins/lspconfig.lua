10f10ffd10e5fa869bd3db7ef5b133a514d4bdbe
Svyatoslav <slava30727@github.com>
2024-12-12 20:30:53 +0000

    use mason's clangd instead of taking it from /usr/bin

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -75,7 +75,7 @@ return {
         })
 
         lspconfig.clangd.setup({
-            cmd = { "/usr/bin/clangd" },
+            cmd = { "clangd" },
             filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
             root_dir = lspconfig.util.root_pattern(".clangd", ".clang-tidy", ".clang-format", "compile_commands.json"),
             single_file_support = true,
