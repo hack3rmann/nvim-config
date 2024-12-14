19ca5b963b143e82014aacb2e9e81c08ad2c7f58
Svyatoslav <slava-varov3@mail.ru>
2024-12-15 00:41:59 +0300

    add "dismiss notifications" keymap

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,3 +1,7 @@
 return {
     "rcarriga/nvim-notify",
+    config = function()
+        local notify = require("notify")
+        vim.keymap.set("n", "<leader>n", notify.dismiss, { desc = "Dismiss notifications" })
+    end,
 }
