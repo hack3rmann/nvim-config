cb17b4fe1d08fd4f0a72af02f01d2534849bb16b
Svyatoslav <slava-varov3@mail.ru>
2025-03-21 18:01:52 +0300

    make the priority of lsp suggestions higher

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -27,6 +27,11 @@ return {
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lsp = {
+                    score_offset = 100,
+                },
+            },
         },
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
