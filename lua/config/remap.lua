36ad97e086b60ba5932b82a481098a9b0a61aeb3
Svyatoslav <slava-varov3@mail.ru>
2025-04-24 13:22:32 +0300

    add keymaps for copying/pasting from system's clipboard

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+
+-- Copy to regular clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to the clipboard", silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from the clipboard", silent = true })
